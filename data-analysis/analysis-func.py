2ef27fc85ed1370bddd938804b517b20b1d0c290
shreyasriram4 <65442252+shreyasriram4@users.noreply.github.com>
2020-06-13 23:03:54 +0800

    Add files via upload

---
+++ data-analysis/analysis-func.py
@@ -0,0 +1,18 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import csv
+>>> def initAnalysis(x):
+	with open('modified.csv', 'w', newline='') as file:
+		fieldnames = ['District Number', 'Number of Successful Responses', 'Total Number of Incidents', '% of Successful Responses', 'Average Number of Nearby CFVs']
+		writer = csv.DictWriter(file, fieldnames=fieldnames)
+		writer.writeheader()
+		writer.writerow({'District Number': 3, 'Number of Successful Responses': 2, 'Total Number of Incidents': 10, '% of Successful Responses': 0.2, 'Average Number of Nearby CFVs': 0.75})
+		writer.writerow({'District Number': 5, 'Number of Successful Responses': 7, 'Total Number of Incidents': 9, '% of Successful Responses': 77.8, 'Average Number of Nearby CFVs': 2.3})
+		writer.writerow({'District Number': 18, 'Number of Successful Responses': 4, 'Total Number of Incidents': 8, '% of Successful Responses': 50, 'Average Number of Nearby CFVs': 1.4})
+		writer.writerow({'District Number': 20, 'Number of Successful Responses': 8, 'Total Number of Incidents': 12, '% of Successful Responses': 66.7, 'Average Number of Nearby CFVs': 1.8})
+		writer.writerow({'District Number': 22, 'Number of Successful Responses': 0, 'Total Number of Incidents': 6, '% of Successful Responses': 0, 'Average Number of Nearby CFVs': 0.5})
+		writer.writerow({'District Number': 27, 'Number of Successful Responses': 3, 'Total Number of Incidents': 5, '% of Successful Responses': 0.6, 'Average Number of Nearby CFVs': 1.2})
+	return
+
+>>> 
